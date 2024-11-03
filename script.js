073ce3c17140d9b5b6487f3db094129b9f17fd92
Andrew Santos <89900236+Andrew-Santos@users.noreply.github.com>
2024-11-03 12:16:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-L.Routing.control({
-    waypoints: [
-        L.latLng(-23.55052, -46.633308), // São Paulo
-        L.latLng(-22.971964, -46.997955), // Campinas (exemplo)
-        L.latLng(-22.906847, -43.172896) // Rio de Janeiro
-    ],
-    routeWhileDragging: true
-}).addTo(map);
+
