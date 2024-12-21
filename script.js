b30546401bfc7fcdc62166541490ad04243f1b0c
Andrew Santos <89900236+Andrew-Santos@users.noreply.github.com>
2024-12-21 00:26:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(() => console.log('Service Worker registrado com sucesso.'))
+        .catch(err => console.error('Erro ao registrar o Service Worker:', err));
+}
+
+
 let map;
 let markersGroup;
 let userMarker;
